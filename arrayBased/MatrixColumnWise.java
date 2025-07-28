23e621fb241d4a60d72a6e6804d7cd0889b869b2
Pavan Kumar S J <108739680+Pavankumar1947@users.noreply.github.com>
2025-07-28 19:25:17 +0530

    Add files via upload

---
+++ arrayBased/MatrixColumnWise.java
@@ -0,0 +1,27 @@
+package arrayBased;
+
+public class MatrixColumnWise {
+	
+	public static void main(String[] args) {
+		
+		
+		int [][] matrix= {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		
+		
+		int rows =matrix.length;
+		int cols = matrix[0].length;
+		
+		
+		for(int i=0; i< cols; i++) {
+			for(int j=0; j< rows; j++) {
+				System.out.print(matrix[j][i]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
